a8bf3e9f187609fc457dcbfa2b9ab4b73b26926f
JesusNathan <nathan.istace@gmail.com>
2016-01-23 00:22:55 +0100

    Création du projet server

---
+++ JeuWebServer/src/jeuweb/server/extension/JeuWebExtension.java
@@ -0,0 +1,13 @@
+package jeuweb.server.extension;
+
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+public class JeuWebExtension extends SFSExtension{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
